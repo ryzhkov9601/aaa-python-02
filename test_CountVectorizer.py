cfa510caa0dc7ffb1faec7074ced447f2b552c3e
Alexander Ryzhkov <ryzhkov9601@gmail.com>
2022-09-15 17:03:02 +0300

    added fixtures for tests

--- test_CountVectorizer.py
+++ test_CountVectorizer.py
@@ -1,28 +1,37 @@
+import pytest
 import CountVectorizer
 
-corpus = [
-    'Crock Pot Pasta Never boil pasta again',
-    'Pasta Pomodoro Fresh ingredients Parmesan to taste'
-]
 
-feature_names = [
-    'crock', 'pot', 'pasta', 'never', 'boil',
-    'again', 'pomodoro', 'fresh', 'ingredients',
-    'parmesan', 'to', 'taste'
-]
+class Test_CountVectorizer:
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
 
-count_matrix = [
-    [1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-]
+    feature_names = [
+        'crock', 'pot', 'pasta', 'never', 'boil',
+        'again', 'pomodoro', 'fresh', 'ingredients',
+        'parmesan', 'to', 'taste'
+    ]
 
+    count_matrix = [
+        [1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    ]
 
-def test_fit_transform():
-    cv = CountVectorizer.CountVectorizer()
-    assert cv.fit_transform(corpus) == count_matrix
+    @pytest.fixture
+    def cv(self):
+        return CountVectorizer.CountVectorizer()
 
+    @pytest.fixture
+    def cv_fitted(self, cv):
+        cv.fit_transform(self.corpus)
+        return cv
 
-def test_get_feature_names():
-    cv = CountVectorizer.CountVectorizer()
-    cv.fit_transform(corpus)
-    assert cv.get_feature_names() == feature_names
+    def test_fit_transform(self, cv):
+        count_matrix = cv.fit_transform(self.corpus)
+        assert count_matrix == self.count_matrix
+
+    def test_get_feature_names(self, cv_fitted):
+        feature_names = cv_fitted.get_feature_names()
+        assert feature_names == self.feature_names
