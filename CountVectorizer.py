4fc68e28984fba3e8414f507b9c73387c7023451
Alexander Ryzhkov <ryzhkov9601@gmail.com>
2022-09-15 15:59:07 +0300

    added class logic

--- CountVectorizer.py
+++ CountVectorizer.py
@@ -1,9 +1,23 @@
+from collections import Counter
+
+
 class CountVectorizer:
     def __init__(self):
         pass
 
     def fit_transform(self, corpus):
-        pass
+        self._feature_names = dict()
+
+        term_counters = [Counter(text.lower().split()) for text in corpus]
+        for counter in term_counters:
+            self._feature_names.update(dict.fromkeys(counter))
+        self._feature_names = list(self._feature_names)
+
+        result = []
+        for counter in term_counters:
+            result.append([counter[key] for key in self._feature_names])
+
+        return result
 
     def get_feature_names(self):
-        pass
+        return self._feature_names
