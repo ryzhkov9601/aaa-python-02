a656ae06a34d36b68a111917a784a56ebe2cffef
Alexander Ryzhkov <ryzhkov9601@gmail.com>
2022-09-15 18:06:02 +0300

    Revert "added print functions for main"
    This reverts commit baf070c7fb30912416e24743fe5fb2f3cdc1e74c.

--- CountVectorizer.py
+++ CountVectorizer.py
@@ -31,20 +31,3 @@ class CountVectorizer:
         """
 
         return self._feature_names
-
-
-if __name__ == '__main__':
-    corpus = [
-        'Crock Pot Pasta Never boil pasta again',
-        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
-    ]
-    vectorizer = CountVectorizer()
-    count_matrix = vectorizer.fit_transform(corpus)
-
-    print(vectorizer.get_feature_names())
-    # Out: ['crock', 'pot', 'pasta', 'never', 'boil', 'again', 'pomodoro',
-    #       'fresh', 'ingredients', 'parmesan', 'to', 'taste']
-
-    print(count_matrix)
-    # Out: [[1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-    #       [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
